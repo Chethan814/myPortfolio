63b11ade56b30277caadbdaf5c414f59d6d6e6c6
chethan <chethanchethu99339@gmail.com>
2025-01-14 14:29:03 +0530

    add NavBar component for navigation layout

---
+++ src/Components/pages/Nav/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+// import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+
+export default function NavBar() {
+    return (
+        <>
+            <div className="navbar font-nav bg-[#0F1015] text-[12px] text-[#FFFFFF] w-[90vw] mx-auto  pt-5 pb-3  ">
+                <div className=" flex justify-between items-center border border-[#5C5757] rounded-full py-4 px-14 ">
+                <div className="logo">
+                    <span>Chethan KR</span>
+                </div>
+                <div className="Nav">
+                    <ul className="w-[14rem] flex justify-between items-center">
+                        <li>About</li>
+                        <li>Work</li>
+                        <li>Career</li>
+                    </ul>
+                </div>
+                <div className="contact">
+                <i class="fa-sharp fa-solid fa-arrow-right"></i>
+                    <span className="pl-2">Contact</span>
+                </div>
+                </div>
+            </div>
+        </>
+    );
+}
