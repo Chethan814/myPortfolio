4e92616ae77a5634e7cab4fc03b56d6dc46c4af7
chethan <chethanchethu99339@gmail.com>
2025-01-16 22:03:22 +0530

    add AboutSection and CTASection components; integrate into App layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import "./index.css";
 import NavBar from "./Components/pages/Nav/NavBar";
 import Hero from "./Components/pages/Hero/HeroSection";
+import AboutSection from "./Components/pages/About/AboutSection";
+import CTASection from "./Components/pages/CTA/CtaSection";
 
 function App() {
     return (
@@ -10,6 +12,8 @@ function App() {
             <div className="bg-[#0F1015] overflow-x-hidden select-none">
                 <NavBar />
                 <Hero />
+                <AboutSection />
+                <CTASection />
             </div>
         </>
     );
